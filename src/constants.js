b161dae0daf77f81251bd0062e187396bfbf4829
DevDiegoMaximo <d.maximoalbuquerque@gmail.com>
2024-05-04 01:41:28 -0300

    first commit

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const scaleFactor = 4
+
+export const dialogueData ={
+    pc: 'TO DO HERE THE PC PART',
+    "cs-degree": 'This is my Computer Engineering degree',
+    "sofa-table":'',
+    tv: '',
+    bed:'',
+    resume:'This is my resume. <a href="http://github.com/DevDiegoMaximo" target="_blank">Here</a>  ',
+    projects:'',
+    library:'',
+    exit: 'If you want to exit this portifolio, just close the tab.'
+
+}
