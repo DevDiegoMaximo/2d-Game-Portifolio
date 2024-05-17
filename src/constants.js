979802fd69f1e69ef00788d7c9d2803ca5ef9c81
DevDiegoMaximo <d.maximoalbuquerque@gmail.com>
2024-05-17 12:40:02 -0300

    added descriptions

--- src/constants.js
+++ src/constants.js
@@ -2,13 +2,13 @@ export const scaleFactor = 4
 
 export const dialogueData ={
     pc: 'TO DO HERE THE PC PART',
-    "cs-degree": 'This is my Computer Engineering degree',
+    "cs-degree": 'This is my Computer Engineering degree, i put this in a wall because i am so proud of it',
     "sofa-table":'',
-    tv: '',
-    bed:'',
-    resume:'This is my resume. <a href="http://github.com/DevDiegoMaximo" target="_blank">Here</a>  ',
-    projects:'',
-    library:'',
+    tv: 'Here is the place that i watch my soccer team called São Paulo!! :D (tbh, my team is kind stressing me out.)',
+    bed:'TO DO HERE THE BED NOTES',
+    resume:'This is my resume and git hub. <a href="http://github.com/DevDiegoMaximo" target="_blank">Here</a>  ',
+    projects:'This project was made using JS framework called Kaboom and Sprite animations. The entire map is JSON tagged and all the images are animated using this Sprite animation with JS!',
+    library:'I really enjoy reading',
     exit: 'If you want to exit this portifolio, just close the tab.'
 
 }
